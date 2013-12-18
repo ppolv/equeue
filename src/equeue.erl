f4596a583597239c84968f210cdae92d41a9651b
Pablo Polvorin <pablo.polvorin@process-one.net>
2013-12-18 16:52:27 -0300

    Use FIFO for subscribers
    I want to give work to the worker that first subscriber, not
    the last one to subscribe.
    (situation is each worker talks to a particular backend, want
     to distribute load to all when available, rather than to send
     always to one, no matter it is able to cope with load)
    
    This patch has a penalty on queue peformance, but it is acceptable

--- src/equeue.erl
+++ src/equeue.erl
@@ -13,7 +13,7 @@
         queue :: queue(),
         blocked_senders :: queue(),   
         current_size :: integer(),
-        subscribed_workers :: [term()],   %%  [{active_once, blocking, pid()|from(), monitor_ref()}]  subscribers
+        subscribed_workers :: queue(),  %%  [{active_once, blocking, pid()|from(), monitor_ref()}]  subscribers
         workers :: [], %% workers that have registered with us, they are monitored. If there is no worked registered, pushes are rejected.
         ongoing_work :: [] %% start a timer for workers that have requested job. If they don't ask for job again in X time, we kill them as
                            %% a countermeasure for bugs where they might become stuck.
@@ -59,7 +59,7 @@ init([Size]) ->
     {ok, #state{size = Size, 
             queue = queue:new(), 
             current_size = 0, 
-            subscribed_workers = [], 
+            subscribed_workers = queue:new(), 
             blocked_senders=queue:new(), 
             workers=[], 
             ongoing_work = []}}.
@@ -74,7 +74,7 @@ handle_call(get_state, _From, State) ->
         {max_size, Size}, 
         {blocked_senders, length(queue:to_list(Blocked))}, 
         {registered_listeners, length(Registered)}, 
-        {blocked_listeners, length(Subscribed)}],
+        {blocked_listeners, queue:len(Subscribed)}],
     {reply, {ok, Info}, State};
 
 handle_call({register_worker,  Pid}, _From, State = #state{workers = Registered}) ->
@@ -97,16 +97,16 @@ handle_call({push, _Job}, _From, State = #state{workers = []}) ->
         {reply, {error, {no_worker_on_queue, self()}}, State};
 handle_call({push, Job}, _FromPush, State = #state{queue = Q, current_size = 0, subscribed_workers = S}) ->
     %% can't be a '$equeue-mark-completed'
-    case S of
-        [{active_once, Pid, _, KillIfNotDoneIn}|S2] ->
+    case queue:out(S) of
+        {{value, {active_once, Pid, _, KillIfNotDoneIn}}, S2} ->
             NewState = monitor_job(Pid, KillIfNotDoneIn, Job, State),
             Pid ! {job, Job},
             {reply, ok, NewState#state{subscribed_workers = S2}};
-        [{blocking, Pid, From, KillIfNotDoneIn}|S2] ->
+        {{value,{blocking, Pid, From, KillIfNotDoneIn}}, S2} ->
             NewState = monitor_job(Pid, KillIfNotDoneIn, Job, State),
             gen_server:reply(From,{ok, Job}),
             {reply, ok, NewState#state{subscribed_workers = S2}};
-        [] ->
+        {empty, _} ->
             {reply, ok, State#state{queue = queue:in(Job, Q), current_size =1}}
     end;
 handle_call({push, Job}, From, State = #state{queue = Q, current_size = QS, size = Size}) ->
@@ -123,7 +123,8 @@ handle_call({push, Job}, From, State = #state{queue = Q, current_size = QS, size
     end;
 
 handle_call({stop_recv, Pid}, _From, State = #state{subscribed_workers = S}) ->
-        {reply, ok, State#state{subscribed_workers = lists:keydelete(Pid, 2, S)}}.
+        NewS = delete_subscriber_from_queue(Pid, S),
+        {reply, ok, State#state{subscribed_workers = NewS}}.
 
 
 
@@ -139,7 +140,7 @@ handle_info({worker_stuck, Pid}, State = #state{ongoing_work = S}) ->
     end;
 handle_info({'DOWN', _MonitorRef, _, Pid, _}, State = #state{subscribed_workers = S}) ->
     {noreply, State#state{
-            subscribed_workers = lists:keydelete(Pid, 2, S), 
+            subscribed_workers = delete_subscriber_from_queue(Pid, S), 
             workers=lists:keydelete(Pid,1, State#state.workers),
             ongoing_work = lists:keydelete(Pid, 1, State#state.ongoing_work)}};
 
@@ -150,6 +151,9 @@ terminate(_Reason, _State) ->
     ok.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+delete_subscriber_from_queue(Pid, SubscribedWorkers) when is_pid(Pid) ->
+        queue:filter(fun({_, SubscriberPid, _, _}) -> SubscriberPid /= Pid end, SubscribedWorkers).
 
 %% Don't print the job as it could be large, containing many tokens :/.  The info would be in the backtrace anyway.
 kill_worker(Queue, Pid, _Job) ->
@@ -174,9 +178,9 @@ do_request_work({_, Pid, _}=Request, From, State = #state{ongoing_work = L}) ->
 do_request_work2({RequestType, Pid, KillIfNotDoneIn}, From, State = #state{current_size = 0, subscribed_workers = S}) ->
     case RequestType of
         blocking ->
-            {noreply, State#state{subscribed_workers = [{blocking, Pid, From, KillIfNotDoneIn} | S]}};
+            {noreply, State#state{subscribed_workers = queue:in({blocking, Pid, From, KillIfNotDoneIn}, S)}};
         active_once ->
-            {reply, ok, State#state{subscribed_workers = [{active_once, Pid, From, KillIfNotDoneIn} | S]}}
+            {reply, ok, State#state{subscribed_workers = queue:in({active_once, Pid, From, KillIfNotDoneIn},  S)}}
     end;
 do_request_work2({RequestType, Pid, KillIfNotDoneIn}=Req, From, State = #state{queue =Q, current_size = QS, blocked_senders = Blocked}) ->
     NewQS = QS -1,
